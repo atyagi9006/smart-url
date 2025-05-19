0918561b9fd74d8408c470f4a11b704844d52b32
Amit Tyagi <tyagi.amit9006@gmail.com>
2025-05-19 20:14:00 +0530

    Added hosting,routing for api urls and graceful shutdown for service.

--- internal/service/smart_url_svc.go
+++ internal/service/smart_url_svc.go
@@ -1,9 +1,16 @@
-package internal
+package service
 
 import (
+	"context"
+	"net/http"
+	"sync"
+	"time"
+
 	handler "github.com/atyagi9006/smart-url/internal/handlers"
 	"go.uber.org/zap"
 )
+
+const ServiceName string = "smart-url-svc"
 
 type SmartUrlSVC struct {
 	handler *handler.Handler
@@ -18,4 +25,31 @@ func NewSmartUrlSVC(log *zap.Logger) *SmartUrlSVC {
 	return &svc
 }
 
+func (svc *SmartUrlSVC) StartApiService(ctx context.Context, wg *sync.WaitGroup) {
 
+	svc.setupRoutes()
+	wg.Add(1) // avoid race
+	go func() {
+		defer wg.Done()
+		svc.Log.Info("Starting service", zap.String("service", ServiceName))
+
+		go func() {
+			http.ListenAndServe(":8080", nil)
+		}()
+
+		// Graceful shutdown logic
+		<-ctx.Done()
+		svc.Log.Info("Stopping API service", zap.String("service", ServiceName))
+		_, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+	}()
+
+}
+
+func (svc *SmartUrlSVC) setupRoutes() {
+	http.HandleFunc("/shorten", svc.handler.ShortenURLHandler)
+	http.HandleFunc("/metrics", svc.handler.MetricsHandler)
+	http.HandleFunc("/", svc.handler.RedirectHandler)
+
+}
--- start/start.go
+++ start/start.go
@@ -1,3 +1,52 @@
 package start
 
-func Run() {}
+import (
+	"context"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+
+	"github.com/atyagi9006/smart-url/internal/service"
+	"go.uber.org/zap"
+)
+
+func Run() {
+
+	svc := initSmartSvc()
+	ctx, cancelFunc := context.WithCancel(context.Background())
+	wg := &sync.WaitGroup{}
+	// Wait for interrupt signal to gracefully shutdown the server with wait group
+	quit := make(chan os.Signal, 1)
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall.SIGKILL but can't be catch, so don't need add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	// Start the api service
+	svc.StartApiService(ctx, wg)
+
+	<-quit
+	svc.Log.Info("Shutting down " + service.ServiceName)
+	// preProcessor.timerRoutineDone <- struct{}{}
+	cancelFunc() // Signal cancellation to context.Context
+	// Wait for all thread to exit
+	wg.Wait()
+	// We are done
+	svc.Log.Info("Graceful shutdown done for " + service.ServiceName)
+
+}
+
+func initSmartSvc() *service.SmartUrlSVC {
+	// Initialize the logger
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		logger.Fatal("Failed to initialize logger", zap.Error(err))
+		os.Exit(1)
+	}
+	defer logger.Sync()
+
+	// Initialize AwsCloudIntegration
+	svc := service.NewSmartUrlSVC(logger)
+	return svc
+}
